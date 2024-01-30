9720ab0d8d98729665cf409086e78f6309b891d3
Harshit Shukla <harshitratanshukla@gmail.com>
2024-01-30 13:09:07 +0530

    update and insert fix

--- src/main/java/com/ratan/main/routes/FirstRoute.java
+++ src/main/java/com/ratan/main/routes/FirstRoute.java
@@ -5,6 +5,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 import com.ratan.main.processor.ConvertJsonToTaskStatus;
+import com.ratan.main.utils.TaskStatus;
 
 @Component
 public class FirstRoute extends RouteBuilder {
@@ -16,8 +17,34 @@ public class FirstRoute extends RouteBuilder {
 		.to("https://jsonplaceholder.typicode.com/todos")
 		.process(new ConvertJsonToTaskStatus())
 		.split().body().delimiter(",")
-		.to("mybatis:mybatis.ratan.requestInsert?StatementType=InsertList")
-		.log("Completed Update");
-		
+		.setProperty("originalBody", simple("${body}"))
+		.to("mybatis:mybatis.ratan.requestSelectById?StatementType=SelectList")
+		.choice()
+				.when(simple("${body} == ''"))
+					.log("Inserting New Row.....")
+					.log("Body ::: ${body}")
+					.process(exchange->{
+						TaskStatus task = (TaskStatus) exchange.getProperty("originalBody");
+						exchange.getIn().setBody(task);
+					})
+					.doTry()
+						.to("mybatis:mybatis.ratan.requestInsert?StatementType=InsertList")
+						.log("Inserted Sucessfully. ${body}")
+					.doCatch(Exception.class)
+						.log("Exception Occurs !!!! ${exception.message}")
+				.endChoice()
+				.otherwise()
+					.log("Updating the Row.....")
+					.process(exchange->{
+						TaskStatus task = (TaskStatus) exchange.getProperty("originalBody");
+						exchange.getIn().setBody(task);
+					})
+					.doTry()
+						.to("mybatis:mybatis.ratan.requestUpdate?StatementType=Update")
+						.log("Updated Sucessfully. ${body}")
+					.doCatch(Exception.class)
+						.log("Exception Occurs !!!! ${exception.message}")
+				.endChoice()
+		.end();
 	}
 }
